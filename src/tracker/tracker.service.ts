5b36556130770187e779b4461021346122149819
Piotr <15027357+piotrkacala@users.noreply.github.com>
2024-12-12 09:12:58 +0100

    Verify tickets on update

--- src/tracker/tracker.service.ts
+++ src/tracker/tracker.service.ts
@@ -13,34 +13,7 @@ export class TrackerService {
   ) { }
 
   async create(input: CreateTrackerDto): Promise<Tracker> {
-    const category = input.category
-    const tracker: Tracker = new Tracker()
-    tracker.title = input.title
-    tracker.description = input.description
-    tracker.status = input.status
-    tracker.category = category
-
-    if (category === Category.STORY) {
-      //Tickets of the type "Story" can have additional "points" field
-      tracker.points = input.points
-    }
-
-    let parent = null
-    if (input.parentId) {
-      parent = await this.trackerRepository.findOneBy({ id: input.parentId })
-    }
-    if (category === Category.STORY && parent?.category !== Category.EPIC) {
-      throw new Error('Stories must belong to an Epic')
-    }
-    if (category === Category.SUBTASK && parent?.category !== Category.STORY) {
-      throw new Error('Tasks must belong to a Story')
-    }
-    tracker.parentId = input.parentId
-    if (category === Category.EPIC) {
-      // Epics cannot be nested under any other items
-      tracker.parentId = null
-    }
-
+    const tracker = await this.checkAndTransform(input)
     return this.trackerRepository.save(tracker)
   }
 
@@ -69,15 +42,51 @@ export class TrackerService {
       statusChange !== 1) {
       throw new Error('Illegal status transition')
     }
-    return await this.trackerRepository.save({
+
+    const tracker = await this.checkAndTransform({
       ...ticket,
-      ...input,
-      created_date: ticket.created_date,
-      updated_date: new Date()
-    });
+      ...input
+    })
+    tracker.id = id
+    tracker.updated_date = new Date()
+
+    return this.trackerRepository.save(tracker)
   }
 
   remove(id: number): Promise<DeleteResult> {
     return this.trackerRepository.delete({ id })
+  }
+
+  private async checkAndTransform(input: CreateTrackerDto) {
+    const category = input.category
+    const tracker: Tracker = new Tracker()
+    tracker.title = input.title
+    tracker.description = input.description
+    tracker.status = input.status
+    tracker.category = category
+
+    if (category === Category.STORY) {
+      //Tickets of the type "Story" can have additional "points" field
+      tracker.points = input.points
+    } else {
+      tracker.points = null
+    }
+
+    let parent = null
+    if (input.parentId) {
+      parent = await this.trackerRepository.findOneBy({ id: input.parentId })
+    }
+    if (category === Category.STORY && parent?.category !== Category.EPIC) {
+      throw new Error('Stories must belong to an Epic')
+    }
+    if (category === Category.SUBTASK && parent?.category !== Category.STORY) {
+      throw new Error('Tasks must belong to a Story')
+    }
+    tracker.parentId = input.parentId
+    if (category === Category.EPIC) {
+      // Epics cannot be nested under any other items
+      tracker.parentId = null
+    }
+    return tracker
   }
 }
